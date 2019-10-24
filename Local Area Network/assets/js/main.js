4227120380e736f3f89c6f19dea57c62d704ed72
Lina <linachan2498@gmail.com>
2019-10-25 09:39:14 +1100

    new flags

---
+++ Local Area Network/assets/js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	console.log("The page is ready!");
+
+	$('.listing').click(function(){
+		$(this).toggleClass('large');
+	});
+
+	$('.nav-item').click(function(){
+		$('.nav-item').removeClass('active');
+		$(this).addClass('active');
+	});
+
+	var hideAll = function(){
+		$('.research-item').addClass('hide');
+	}
+
+	$('.btn-September').click(function(){
+		hideAll();
+		$('.September').removeClass('hide');
+	});
+
+	$('.btn-October').click(function(){
+		hideAll();
+		$('.October').removeClass('hide');
+	});
+
+	$('.btn-November').click(function(){
+		hideAll();
+		$('.November').removeClass('hide');
+	});
+
+	$('.btn-December').click(function(){
+		hideAll();
+		$('.December').removeClass('hide');
+	});
+
+});
